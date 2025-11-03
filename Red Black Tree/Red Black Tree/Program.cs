4509b241f7d2f573c2d981875379f187d4ba2e5d
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-03 18:41:51 +0100

    Initial Commit

---
+++ Red Black Tree/Red Black Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+
+	public class RedBlackTree
+	{
+		private Node root;
+
+
+		// Node definition for the Red-Black Tree
+		public class Node
+		{
+
+
+			public int Value;
+			public Node Left, Right, Parent;
+			public byte IsRed = 0; // New nodes are red by default
+
+			public Node(int value)
+			{
+				Value = value;
+			}
+		}
+
+
+
+	}
+	private static void Main(string[] args)
+	{
+
+	}
+}
