50a9482d227c9a651d9c0973491d94bdff605bd1
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-05 19:49:51 +0100

    Add Insert Method

--- Red Black Tree/Red Black Tree/Program.cs
+++ Red Black Tree/Red Black Tree/Program.cs
@@ -13,7 +13,7 @@
 
 			public int Value;
 			public Node Left, Right, Parent;
-			public byte IsRed = 0; // New nodes are red by default
+			public bool IsRed = true; // New nodes are red by default
 
 			public Node(int value)
 			{
@@ -21,8 +21,35 @@
 			}
 		}
 
+		// Public method to insert a value into the tree
+		public void Insert(int newValue)
+		{
+			Node newNode = new Node(newValue);
+			// handle the special case when the tree is empty
 
+			if (root == null)
+			{
+				root = newNode;
+				newNode.IsRed = false;
+				return;
+			}
 
+			// Standar Binary Search Tree Insertion
+
+			Node current = root;
+			Node parent = null;
+
+			while (current != null)
+			{
+				parent = current;
+				if (current.Value < newValue)
+					current = current.Left;
+				else if (current.Value > newValue)
+					current = current.Right;
+
+			}
+
+		}
 	}
 	private static void Main(string[] args)
 	{
