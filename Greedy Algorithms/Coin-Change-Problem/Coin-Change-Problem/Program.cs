2e8fe10ba22b1b6f582fd6613063eb585ecac283
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-29 14:49:42 +0100

    Coin-Change-Problem

---
+++ Greedy Algorithms/Coin-Change-Problem/Coin-Change-Problem/Program.cs
@@ -0,0 +1,59 @@
+﻿class Coin
+{
+	public int Amount { get; set; }
+	public List<int>? Coins { get; set; }
+
+	public Coin(int amount, List<int> coins)
+	{
+		Amount = amount;
+		Coins = coins;
+	}
+
+	public List<int> Discount()
+	{
+		// Return if there is no coins to change
+		if (Coins == null) return new List<int>();
+
+		// Sort the list Desn
+		Coins.Sort((a, b) => b - a);
+
+		// A list to store the result
+		List<int> result = new List<int>();
+
+		foreach (var coin in Coins)
+		{
+			while (Amount >= coin)
+			{
+				Amount -= coin;
+				result.Add(coin);
+			}
+		}
+
+
+		return result;
+	}
+
+
+	public static void Main()
+	{
+		// Define list of coins to change
+		var coins = new List<int> { 1, 5, 10, 20, 50, 100 };
+		var coin = new Coin(33, coins);
+
+		// Call Discount method
+		var result = coin.Discount();
+
+
+		// Print the result
+		Console.WriteLine("Coins to change :");
+
+		foreach (int item in result)
+		{
+			Console.WriteLine(item);
+		}
+
+		Console.WriteLine($"Total coins: {result.Count}");
+
+		Console.ReadKey();
+	}
+}
