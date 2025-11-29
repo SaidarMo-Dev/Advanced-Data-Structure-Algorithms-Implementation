759389f30db55cc4c597006b1bd0fbf82b45e0eb
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-29 14:50:41 +0100

    Palindrome Queue

---
+++ Queue-Stack Problems/Palindrome-Queue/Palindrome-Queue/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Queue<int> queue = new Queue<int>([1, 2, 3, 2, 1]);
+
+		Console.WriteLine("Checking if the queue is Palindrome:");
+		Console.WriteLine(IsPalindrome(queue) ? "the queue is Palindrome" : "the queue is not Palindrome");
+	}
+
+	public static bool IsPalindrome(Queue<int> queue)
+	{
+		Stack<int> stack = new(queue);
+
+		while (stack.Count > 0)
+		{
+			if (queue.Dequeue() != stack.Pop()) return false;
+
+		}
+
+		return true;
+	}
+}
