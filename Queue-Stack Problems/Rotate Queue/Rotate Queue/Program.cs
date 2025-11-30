c1649c2cf2f789d38417ba82ddbd45280109b6cd
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-30 18:26:21 +0100

    Rotate Queue

---
+++ Queue-Stack Problems/Rotate Queue/Rotate Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Queue<int> queue = new Queue<int>([1, 2, 3, 4, 5]);
+		int k = 2;
+
+		Console.WriteLine($"Before Rotation by {k}:");
+		Console.WriteLine(string.Join(",", queue.ToArray()));
+
+
+		queue = RotateQueue(queue, k);
+		Console.WriteLine($"\nAfter Rotation by {k}:");
+		Console.WriteLine(string.Join(",", queue.ToArray()));
+
+	}
+
+	public static Queue<int> RotateQueue(Queue<int> queue, int k)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			queue.Enqueue(queue.Dequeue());
+		}
+
+		return queue;
+	}
+}
