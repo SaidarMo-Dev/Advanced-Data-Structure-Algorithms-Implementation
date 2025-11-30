05bdc2d23b825b709fef8f2b8672ac541edaca40
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-30 18:25:12 +0100

    Interleave Queue Elements

---
+++ Queue-Stack Problems/Interleave Queue Elements/Interleave Queue Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+
+		Queue<int> queue = new Queue<int>([1, 2, 3, 4, 5, 6, 72, 23, 31]);
+
+		Console.WriteLine("After interleave elements:");
+		queue = InterleaveElements(queue);
+		Console.WriteLine(string.Join(",", queue.ToArray()));
+		Console.ReadKey();
+	}
+
+	public static Queue<int> InterleaveElements(Queue<int> queue)
+	{
+		var halfQueue = new Queue<int>();
+
+		int halfSize = queue.Count / 2;
+
+		for (int i = 0; i < halfSize; i++)
+		{
+			halfQueue.Enqueue(queue.Dequeue());
+		}
+
+		for (int i = 0; i < halfSize; i++)
+		{
+			queue.Enqueue(halfQueue.Dequeue());
+			queue.Enqueue(queue.Dequeue());
+		}
+
+
+		return queue;
+	}
+}
