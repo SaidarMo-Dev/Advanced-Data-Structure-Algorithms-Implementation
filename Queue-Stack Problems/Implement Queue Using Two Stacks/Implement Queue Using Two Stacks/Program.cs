53489a44533be9c84c00a31c301b8227db5c9e2e
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-30 18:23:16 +0100

    Implement Queue Using Two Stacks

---
+++ Queue-Stack Problems/Implement Queue Using Two Stacks/Implement Queue Using Two Stacks/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+
+	class MyQueue<T>
+	{
+		Stack<T> Incomming = new();
+		Stack<T> Outcomming = new();
+
+		public int Count
+		{
+			get
+			{
+				return Incomming.Count + Outcomming.Count;
+			}
+		}
+		public void Enqueue(T value)
+		{
+			Incomming.Push(value);
+		}
+		public T Dequeue()
+		{
+			if (Outcomming.Count == 0)
+			{
+				while (Incomming.Count > 0)
+				{
+					Outcomming.Push(Incomming.Pop());
+				}
+			}
+
+			return Outcomming.Pop();
+
+
+		}
+
+	}
+
+	private static void Main(string[] args)
+	{
+		MyQueue<int> queue = new MyQueue<int>();
+
+		queue.Enqueue(11);
+		queue.Enqueue(22);
+		queue.Enqueue(33);
+		queue.Enqueue(34);
+
+		int length = queue.Count;
+		for (int i = 0; i < length; i++)
+		{
+			Console.WriteLine($"Dequeue element {i + 1}:{queue.Dequeue()}");
+
+		}
+
+		Console.ReadKey();
+	}
+}
