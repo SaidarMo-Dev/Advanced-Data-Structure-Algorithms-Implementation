6e3a6f81f3901e28ce8514fdbf45331c706cc6f8
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-30 18:22:29 +0100

    Generate Binary Numbers

---
+++ Queue-Stack Problems/Generate Binary Numbers/Generate Binary Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Enter n numbers to convert to Binary:");
+		string? n = Console.ReadLine();
+
+		Console.WriteLine("Numbers Converted are:");
+		var numbersToBinray = GenerateBinaryNumbers(Convert.ToInt32(n));
+		foreach (var number in numbersToBinray)
+		{
+			Console.WriteLine(number);
+		}
+		Console.ReadKey();
+	}
+
+	public static string[] GenerateBinaryNumbers(int n)
+	{
+		string[] numbers = new string[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			int number = i + 1;
+
+			Queue<int> queue = new Queue<int>();
+			while (number > 0)
+			{
+				queue.Enqueue(number % 2);
+				number /= 2;
+			}
+
+			numbers[i] = string.Join("", queue.ToArray());
+		}
+
+		return numbers;
+	}
+}
