9eb33af94bf47c547b474efb1e8beb41200ad782
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-11-30 18:26:53 +0100

    Sort Queue

---
+++ Queue-Stack Problems/Sort Queue/Sort Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Queue<int> queue = new Queue<int>([1, 3, 2, 5, 4]);
+
+		queue = SortQueue(queue);
+		Console.WriteLine("After sorting the queue:");
+
+		foreach (int i in queue)
+		{
+			Console.WriteLine(i);
+		}
+	}
+
+	public static Queue<int> SortQueue(Queue<int> queue)
+	{
+		var list = new List<int>(queue);
+
+		list.Sort((a, b) => a - b);
+
+		return new(list);
+	}
+}
