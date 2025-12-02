21ad49022764c44352ddfee03e54b337b82305aa
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 18:42:40 +0100

    Add More Problems

---
+++ Median of Two Sorted Arrays/Median of Two Sorted Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+
+	public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+	{
+		// merge arrays 
+		var arr = nums1.Concat(nums2).ToList();
+
+		arr.Sort();
+
+		int m = nums1.Length;
+		int n = nums2.Length;
+
+		int length = n + m;
+
+		if (length % 2 != 0) return arr[length / 2];
+
+		int middle = length / 2;
+
+		return ((double)(arr[middle - 1] + arr[middle])) / 2;
+
+	}
+	private static void Main(string[] args)
+	{
+		Console.WriteLine(FindMedianSortedArrays([2, 4,], [1, 5]));
+	}
+}
---
+++ Queue-Stack Problems/Find First Non-Repeating Character/Find First Non-Repeating Character/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.ReadKey();
+	}
+	public static void FindFirstNonRepaetingCharacter(string stream)
+	{
+		Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+		foreach (var character in stream)
+		{
+			if (!dictionary.ContainsKey(character.ToString()))
+				dictionary[character.ToString()] = 0;
+
+			dictionary[character.ToString()] += 1;
+
+			if (dictionary[character.ToString()] > 1)
+				Console.WriteLine(character);
+			else
+				Console.WriteLine("-");
+
+		}
+	}
+
+
+}
