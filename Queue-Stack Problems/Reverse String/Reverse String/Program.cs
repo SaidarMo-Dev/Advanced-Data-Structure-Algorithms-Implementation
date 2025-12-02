309dae9d6e09f196d5111c577409db7ae04c2f57
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 18:36:58 +0100

    Reverse String

---
+++ Queue-Stack Problems/Reverse String/Reverse String/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+
+	public static string ReverseString(string stream)
+	{
+		var stack = new Stack<char>();
+
+		foreach (char chr in stream)
+		{
+			stack.Push(chr);
+		}
+
+		string reversedString = "";
+
+		while (stack.Count > 0)
+		{
+
+			reversedString += stack.Pop();
+		}
+
+		return reversedString;
+	}
+
+	private static void Main(string[] args)
+	{
+		string stream = "Test string";
+
+		var result = ReverseString(stream);
+
+		Console.WriteLine($"Original string is :{stream}");
+		Console.WriteLine($"Reversed string:{result}");
+
+		Console.ReadKey();
+	}
+
+
+}
