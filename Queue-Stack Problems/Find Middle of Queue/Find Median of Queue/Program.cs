c9c4f161075c83e9aca8ef9d0ba90880b3fd5e0a
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 18:37:41 +0100

    Find Middle of Queue

---
+++ Queue-Stack Problems/Find Middle of Queue/Find Median of Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var q = new Queue<int>([1, 2, 3, 5]);
+
+		Console.WriteLine($"The Median of the queue is :{FindMedian(q)}");
+	}
+
+	public static int FindMedian(Queue<int> queue)
+	{
+
+		var list = new List<int>(queue);
+
+		return list[list.Count / 2];
+	}
+}
