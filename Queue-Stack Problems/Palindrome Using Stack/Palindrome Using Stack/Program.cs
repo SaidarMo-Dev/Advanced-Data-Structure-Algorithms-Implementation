342817a3fc4e9f1e02ba025631a86d40d49d110c
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 21:48:06 +0100

    Palindrome Using Stack

---
+++ Queue-Stack Problems/Palindrome Using Stack/Palindrome Using Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		string s = "Hello, my name is mohammed saidar";
+
+		Console.WriteLine(IsPalindrome(s));
+
+		Console.ReadKey();
+	}
+
+	public static bool IsPalindrome(string s)
+	{
+		Stack<char> stack = new(s);
+
+		foreach (char c in s)
+		{
+			if (c != stack.Pop()) return false;
+		}
+
+		return true;
+
+	}
+}
