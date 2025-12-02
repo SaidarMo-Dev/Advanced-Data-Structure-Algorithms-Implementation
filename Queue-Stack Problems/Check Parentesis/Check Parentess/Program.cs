8afb0f92ff172293deb909c6dcc9658927dcf0f6
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 21:38:48 +0100

    Check Parentesis

---
+++ Queue-Stack Problems/Check Parentesis/Check Parentess/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+	public static bool IsParenstesisValide(string s)
+	{
+		Stack<char> stack = new();
+		Dictionary<char, char> parentesis = new Dictionary<char, char>
+		{
+			{ ')', '(' },
+			{ '}', '{' },
+			{ ']', '[' },
+		};
+
+
+		foreach (char c in s)
+		{
+			if (parentesis.ContainsValue(c))
+				stack.Push(c);
+
+			else if (parentesis.ContainsKey(c))
+			{
+				if (stack.Count == 0 || stack.Pop() != parentesis[c])
+					return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	private static void Main(string[] args)
+	{
+		string parentess = "(){}";
+
+		Console.WriteLine(IsParenstesisValide(parentess));
+
+		Console.ReadKey();
+	}
+}
