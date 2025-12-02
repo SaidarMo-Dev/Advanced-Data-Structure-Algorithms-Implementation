96a387c8e7efeb1198b9a5cabf1d54c496057406
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-02 18:39:06 +0100

    lone Queue

---
+++ Queue-Stack Problems/Clone Queue/Clone Queue/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var q = new Queue<int>([1, 2, 3, 4, 5]);
+
+		var clonedqueue = CloneQueue(q);
+
+		var qLength = q.Count;
+
+		Console.WriteLine($"Origin queue Elements: {string.Join(",", q)} ");
+		Console.WriteLine($"cloned queue Elements: {string.Join(",", clonedqueue)}");
+
+		Console.ReadKey();
+	}
+
+	public static Queue<int> CloneQueue(Queue<int> queue)
+	{
+		var clonedQueue = new Queue<int>();
+
+		var length = queue.Count;
+
+		for (int i = 0; i < length; i++)
+		{
+			var front = queue.Dequeue();
+
+			clonedQueue.Enqueue(front);
+
+			queue.Enqueue(front);
+		}
+
+		return clonedQueue;
+	}
+
+}
