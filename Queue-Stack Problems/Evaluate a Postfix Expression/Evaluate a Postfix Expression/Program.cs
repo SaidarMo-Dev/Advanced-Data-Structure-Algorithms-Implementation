977612c0c5ac6e9b165c55ac221bb30ff4c96227
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-04 18:20:14 +0100

    Evaluate a Postfix Expression

---
+++ Queue-Stack Problems/Evaluate a Postfix Expression/Evaluate a Postfix Expression/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+	public static int EvaluatePostfixExpr(string input)
+	{
+		Stack<int> stack = new Stack<int>();
+
+		foreach (char chr in input)
+		{
+			if (char.IsDigit(chr))
+				stack.Push(chr - '0');
+
+			else
+			{
+				int a = stack.Pop();
+				int b = stack.Pop();
+
+				switch (chr)
+				{
+					case '+':
+						stack.Push(b + a);
+						break;
+					case '-':
+						stack.Push(b - a);
+						break;
+					case '*':
+						stack.Push(b * a);
+						break;
+					case '/':
+						stack.Push(b / a);
+						break;
+				}
+			}
+		}
+		return stack.Pop();
+
+	}
+
+	private static void Main(string[] args)
+	{
+		string s = "231*+9-";
+
+		Console.WriteLine(EvaluatePostfixExpr(s));
+		Console.ReadKey();
+	}
+}
