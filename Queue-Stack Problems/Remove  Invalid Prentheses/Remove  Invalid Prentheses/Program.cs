963d22a4a024868bbbd371a27b0ec31a628a1113
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-04 18:20:44 +0100

    Remove  Invalid Prentheses

---
+++ Queue-Stack Problems/Remove  Invalid Prentheses/Remove  Invalid Prentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+	public static string RemoveInvalidParentheses(string s)
+	{
+
+		Stack<int> stack = new();
+		HashSet<int> invalidIndices = new();
+
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (s[i] == '(') stack.Push(i);
+
+			else if (s[i] == ')')
+			{
+				if (stack.Count == 0) invalidIndices.Add(i);
+				else stack.Pop();
+
+			}
+
+		}
+
+		while (stack.Count > 0)
+		{
+			invalidIndices.Add(stack.Pop());
+		}
+
+
+		string newString = "";
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (!invalidIndices.Contains(i))
+			{
+				newString += s[i];
+			}
+		}
+
+		return newString;
+	}
+
+
+	private static void Main(string[] args)
+	{
+		string s = "((((((())";
+
+		Console.WriteLine($"After remove invalid parentheses:{RemoveInvalidParentheses(s)}");
+		Console.ReadKey();
+	}
+
+}
