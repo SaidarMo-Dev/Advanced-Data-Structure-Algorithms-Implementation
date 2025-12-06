c98dc437afe55d11b252b05b09d35951c6a08ad7
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-06 18:52:35 +0100

    Count Words Frequencies in a Text

---
+++ Dictionary-Hashset Problems/Count Words Frequencies in a Text/Count Words Frequencies in a Text/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+	public static Dictionary<string, int> CountWordFrequencies(string text)
+	{
+		Dictionary<string, int> frequencies = new Dictionary<string, int>();
+
+		string[] words = text.Split(" ");
+
+		foreach (string word in words)
+		{
+			if (!frequencies.ContainsKey(word)) frequencies[word] = 1;
+			else frequencies[word]++;
+		}
+
+		return frequencies;
+	}
+
+	private static void Main(string[] args)
+	{
+		string text = "hello my name is mohammed mohammed hello name";
+
+		Dictionary<string, int> result = CountWordFrequencies(text);
+
+		foreach (var (key, value) in result)
+		{
+			Console.WriteLine($"{key}:{value}");
+		}
+
+		Console.ReadKey();
+	}
+}
