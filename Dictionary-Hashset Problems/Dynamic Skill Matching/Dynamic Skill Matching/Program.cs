e6c720d00167bb9603135f791104d58396dbd04b
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-06 18:52:02 +0100

    Dynamic Skill Matching

---
+++ Dictionary-Hashset Problems/Dynamic Skill Matching/Dynamic Skill Matching/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		HashSet<string> candidateSkills = new HashSet<string> { "C#", "Javascript", "SQL", "T-SQL" };
+
+		HashSet<string> jobRequirements = new HashSet<string> { "C#", "SQL", "C++" };
+
+		candidateSkills.IntersectWith(jobRequirements);
+
+		Console.Write($"Matching skills :{string.Join(",", candidateSkills)}");
+
+		Console.ReadKey();
+	}
+
+
+}
