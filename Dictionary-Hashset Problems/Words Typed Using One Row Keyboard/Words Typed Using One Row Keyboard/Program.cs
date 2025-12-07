1f6be3d7e3b7c4379913a9508d3d1f4422fe3c53
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-07 16:59:23 +0100

    Find Missing Number

---
+++ Dictionary-Hashset Problems/Words Typed Using One Row Keyboard/Words Typed Using One Row Keyboard/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+	public static List<string> FindWordsTypedWithOneRow(string[] words)
+	{
+		string[] rows = { "qwertzuiop", "asdfghjkl", "yxcvbnm" };
+		Dictionary<char, int> charRow = new Dictionary<char, int>();
+
+		for (int i = 0; i < rows.Length; i++)
+		{
+			foreach (char c in rows[i])
+			{
+				charRow[c] = i;
+			}
+		}
+
+		List<string> validWords = new();
+
+		foreach (string word in words)
+		{
+			int row = charRow[char.ToLower(word[0])];
+			bool valid = true;
+
+			foreach (char c in word)
+			{
+				if (charRow[char.ToLower(c)] != row)
+				{
+					valid = false;
+					break;
+				}
+			}
+
+			if (valid) validWords.Add(word);
+		}
+
+		return validWords;
+	}
+
+	private static void Main(string[] args)
+	{
+		string[] words = ["Hello", "Ali", "Power", "Asad"];
+
+		Console.WriteLine($"Words that typed using one row of keyboard are: {string.Join(",", FindWordsTypedWithOneRow(words))}");
+
+
+		Console.ReadKey();
+	}
+}
