e09c0c97ccb8e48e74177630133bcd86e5def3b6
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-07 14:37:37 +0100

    Find Duplicate Elements

---
+++ Dictionary-Hashset Problems/Find Duplicate Elements/Find Duplicate Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+	public static int[] DuplicateElements(int[] nums)
+	{
+		Dictionary<int, int> counts = new Dictionary<int, int>();
+		List<int> duplicate = new List<int>();
+
+		foreach (int num in nums)
+		{
+			if (counts.ContainsKey(num))
+			{
+				counts[num]++;
+
+				if (counts[num] == 2) duplicate.Add(num);
+			}
+			else
+				counts[num] = 1;
+
+		}
+
+		return duplicate.ToArray();
+	}
+	private static void Main(string[] args)
+	{
+		int[] nums = { 1, 2, 2, 3, 3, 4 };
+
+
+		Console.WriteLine($"Duplicate ELements: {string.Join(",", DuplicateElements(nums))}");
+		Console.ReadKey();
+	}
+}
