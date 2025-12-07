1c8ee7c12e11d41980c812b7cdfc18d6fc8c7cca
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-07 14:37:03 +0100

    Find All Unique Elements

---
+++ Dictionary-Hashset Problems/Find All Unique Elements/Find All Unique Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+	public static int[] UniqueElements(int[] nums)
+	{
+		Dictionary<int, int> counts = new Dictionary<int, int>();
+
+		foreach (int num in nums)
+		{
+			if (!counts.ContainsKey(num)) counts[num] = 0;
+
+			counts[num]++;
+		}
+
+		List<int> unique = new();
+
+		foreach (var kvp in counts)
+		{
+			if (kvp.Value == 1) unique.Add(kvp.Key);
+		}
+
+		return unique.ToArray();
+	}
+
+
+	private static void Main(string[] args)
+	{
+		int[] nums = { 1, 1, 2, 3 };
+
+		Console.WriteLine($"Unique Elements: [{string.Join(",", UniqueElements(nums))}]");
+
+		Console.ReadKey();
+	}
+}
