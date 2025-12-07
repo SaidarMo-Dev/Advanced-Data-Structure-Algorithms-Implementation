dbba358db0c0c3be2d2fc22a948fd5526cec0913
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-07 16:58:28 +0100

    Find Missing Number

---
+++ Dictionary-Hashset Problems/Find Missing Number/Find Missing Number/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+	public static int MissingNumber(int[] nums)
+	{
+		Dictionary<int, bool> counts = new Dictionary<int, bool>();
+
+		foreach (int i in nums)
+		{
+			counts[i] = true;
+		}
+
+		for (int i = 0; i <= nums.Length; i++)
+		{
+			if (!counts.ContainsKey(i))
+				return i;
+
+		}
+
+		return -1;
+	}
+	private static void Main(string[] args)
+	{
+		int[] numbers = { 0, 2, 3 };
+
+		int missingN = MissingNumber(numbers);
+
+		Console.WriteLine("Missing number is:" + (missingN != -1 ? missingN : "No Number Missing"));
+		Console.ReadKey();
+	}
+}
