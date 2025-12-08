6f476dbfe5ad16aa4b5d0263812724b908e1a923
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-08 21:29:42 +0100

    Numbers Disappeared in an Array

---
+++ Dictionary-Hashset Problems/Numbers Disappeared in an Array/Numbers Disappeared in an Array/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+	public static List<int> DisappearedNumbers(int[] nums)
+	{
+		HashSet<int> presentNums = new(nums);
+
+
+		List<int> result = new();
+
+		for (int i = 1; i <= nums.Length; i++)
+		{
+			if (!presentNums.Contains(i)) result.Add(i);
+		}
+
+		return result;
+	}
+	private static void Main(string[] args)
+	{
+		int[] nums = { 23, 22, 4, 5, 6 };
+		Console.WriteLine($"Missing numbers are:{string.Join(",", DisappearedNumbers(nums))}");
+
+
+		Console.ReadKey();
+	}
+}
