169b36c79e9f528bdffa6de1b00d54f73d88afff
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-08 21:26:57 +0100

    Check if Two Arrays are Disjoint

---
+++ Dictionary-Hashset Problems/Check if Two Arrays are Disjoint/Check if Two Arrays are Disjoint/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+	public static bool DisjointArrays(int[] arr1, int[] arr2)
+	{
+
+		// Using Hashset for quick loockups
+		HashSet<int> set2 = [.. arr2];
+
+
+		foreach (int num in arr1)
+		{
+			if (set2.Contains(num)) return false;
+		}
+
+		return true;
+	}
+
+	private static void Main(string[] args)
+	{
+		int[] arr1 = { 1, 2, 3 }, arr2 = { 4, 5, 6 };
+
+		Console.WriteLine(DisjointArrays(arr1, arr2));
+
+		Console.ReadKey();
+	}
+}
