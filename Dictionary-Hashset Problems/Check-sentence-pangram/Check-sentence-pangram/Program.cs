80f295f2d7038597e9833924fd859da44e951231
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-12 19:29:45 +0100

    Check-sentence-pangram/

---
+++ Dictionary-Hashset Problems/Check-sentence-pangram/Check-sentence-pangram/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+	public static bool SentencePangram(string sentence)
+	{
+
+		HashSet<char> chars = new HashSet<char>();
+
+		foreach (char c in sentence)
+		{
+			if (char.IsLetter(c)) chars.Add(c);
+		}
+
+		return chars.Count == 26;
+	}
+
+	private static void Main(string[] args)
+	{
+		string sentence = "The quick brown fox jumps over the lazy d";
+
+		Console.WriteLine(SentencePangram(sentence));
+		Console.ReadKey();
+	}
+}
