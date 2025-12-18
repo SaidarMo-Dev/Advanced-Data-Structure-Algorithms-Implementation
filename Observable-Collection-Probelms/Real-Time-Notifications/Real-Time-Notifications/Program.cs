f483d123c7bc027a8ce624456ff81e5a7ca4e93b
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-18 20:41:32 +0100

    Real-Time-Notifications

---
+++ Observable-Collection-Probelms/Real-Time-Notifications/Real-Time-Notifications/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		ObservableCollection<string> notifications = new ObservableCollection<string>();
+
+		notifications.CollectionChanged += (s, e) =>
+		{
+			if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+				Console.WriteLine("New Notification:" + e.NewItems[0]);
+		};
+
+		notifications.Add("Your Task under processing");
+		notifications.Add("Your Task completed");
+
+
+		Console.ReadKey();
+	}
+}
