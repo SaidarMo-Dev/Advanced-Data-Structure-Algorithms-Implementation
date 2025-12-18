5f3a87fe286ef75776d21cbc63d55cd3159b4788
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-18 20:41:00 +0100

    Live-Chat-Application

---
+++ Observable-Collection-Probelms/Live-Chat-Application/Live-Chat-Application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		ObservableCollection<string> messages = new ObservableCollection<string>();
+
+		messages.CollectionChanged += (sender, e) =>
+		{
+			if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+				Console.WriteLine($"New Message: {e.NewItems[0]}");
+
+		};
+
+
+		messages.Add("Hello mohammed");
+		messages.Add("How Are you");
+
+
+		Console.ReadKey();
+	}
+}
