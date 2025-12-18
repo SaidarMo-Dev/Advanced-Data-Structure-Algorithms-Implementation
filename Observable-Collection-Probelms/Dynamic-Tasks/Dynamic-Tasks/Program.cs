00d0572f393a6994c730878f0ec58549db117882
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-18 20:40:34 +0100

    Dynamic Tasks

---
+++ Observable-Collection-Probelms/Dynamic-Tasks/Dynamic-Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		ObservableCollection<string> tasks = new ObservableCollection<string>();
+
+		tasks.CollectionChanged += (sender, e) =>
+		{
+			// You an log to Db or external files instead of output it in the console
+
+			if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+				Console.WriteLine($"New Task Added:{e.NewItems[0]}");
+			else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+				Console.WriteLine($"Task Removed:{e.OldItems[0]}");
+
+		};
+
+		tasks.Add("Attend Metting");
+		tasks.Add("Completed Metting");
+		tasks.Add("Report Completed");
+
+		tasks.Remove("Attend Metting");
+
+		Console.ReadKey();
+	}
+}
