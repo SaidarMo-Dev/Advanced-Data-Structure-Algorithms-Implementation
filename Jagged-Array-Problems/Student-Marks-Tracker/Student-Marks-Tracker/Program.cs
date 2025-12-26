532eeeab9efbd55f219ec3a5a767b9a5649b5fc0
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-26 19:47:59 +0100

    Student-Marks-Tracker

---
+++ Jagged-Array-Problems/Student-Marks-Tracker/Student-Marks-Tracker/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+
+		int[][] studentsMarks = new int[][]
+		{
+			[85, 90, 95], // Marks for Student 1
+			[83, 92, 91, 88], // Marks for Student 2
+			[85, 90] // Marks for Student 3
+	};
+
+		for (int i = 0; i < studentsMarks.Length; i++)
+		{
+			Console.WriteLine($"student {i + 1} marks: {string.Join(",", studentsMarks[i])}");
+		}
+
+		Console.ReadKey();
+	}
+}
