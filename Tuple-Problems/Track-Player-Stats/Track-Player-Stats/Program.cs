34414248effec8e6554b169c331b68f66549e956
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-12-30 21:50:05 +0100

    Track-Player-Stats

---
+++ Tuple-Problems/Track-Player-Stats/Track-Player-Stats/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	public static void DisplayPlayers(List<(string name, int age, int helth)> players)
+	{
+		foreach (var player in players)
+		{
+			Console.WriteLine($"Player:{player.name},Age:{player.age}, helth:{player.helth}");
+		}
+	}
+	private static void Main(string[] args)
+	{
+		List<(string name, int age, int helth)> players = new List<(string name, int age, int helth)>
+		{
+			("Mohammed", 25, 99),
+			("Ali", 32, 85),
+			("Ahmed", 22, 45),
+			("Sami", 27, 98),
+		};
+
+
+		DisplayPlayers(players);
+
+	}
+}
