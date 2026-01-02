99d81e49c48a682090b203d45846cf7d6ee8458d
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-02 21:46:14 +0100

    Light-Control-System

---
+++ BitArray-Problems/Light-Control-System/Light-Control-System/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var lights = new BitArray(8, false);
+
+		lights[1] = true;
+		lights[2] = true;
+
+		Console.WriteLine($"Light 2: {(lights[1] ? "On" : "Off")}");
+
+		// Reset All 
+
+		lights.SetAll(false);
+
+		// Light 2 after reseting
+		Console.WriteLine($"Light 2: {(lights[1] ? "On" : "Off")}");
+		Console.ReadKey();
+	}
+}
