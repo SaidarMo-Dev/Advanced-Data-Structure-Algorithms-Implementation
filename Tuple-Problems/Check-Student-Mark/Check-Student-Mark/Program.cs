6737198a02a3244e65ae25cdd63863a4dcb559e0
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-02 20:55:23 +0100

    Check-Student-Mark

---
+++ Tuple-Problems/Check-Student-Mark/Check-Student-Mark/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+	public static (bool Success, int Value) CheckMark(int Mark)
+	{
+		if (Mark >= 50) return (true, Mark);
+
+		return (false, Mark);
+	}
+
+	private static void Main(string[] args)
+	{
+		var result = CheckMark(99);
+
+		Console.WriteLine($"Success:{result.Success}, Value:{result.Value}");
+		Console.ReadKey();
+	}
+}
