61ddf7e895b9f0242e695aeaaa1d7980c0e13847
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-02 21:46:43 +0100

    Scheduiling-Tasks

---
+++ BitArray-Problems/Scheduiling-Tasks/Scheduiling-Tasks/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		BitArray schelue = new BitArray(7, true);
+
+		schelue[3] = false;
+		schelue[5] = false;
+
+		Console.Write("Free Days:");
+		for (int i = 0; i < 7; i++)
+		{
+			if (!schelue[i]) Console.Write($" Day {i + 1}");
+
+
+		}
+
+
+		Console.ReadKey();
+	}
+}
