45c03e075df1b74a67650f186d3ab7b0d78acff5
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-02 20:56:24 +0100

    Store-Compare-Employee

---
+++ Tuple-Problems/Store-Compare-Employee/Store-Compare-Employee/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var employee1 = (name: "Ali", salary: 4000);
+		var employee2 = (name: "mohammed", salary: 6500);
+
+		Console.WriteLine($"{employee1.name} has {(employee1.salary > employee2.salary ? "higher" : "lower")} salary than {employee2.name}");
+		Console.ReadKey();
+	}
+}
