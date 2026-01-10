b0237961677db2641ab756a2d2b04e227af3598d
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-10 20:21:28 +0100

    Voting-System

---
+++ BitArray-Problems/Voting-System/Voting-System/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		BitArray vootings = new BitArray(new bool[] { true, false, true, true, false, false, true, false });
+
+		var yesVotes = 0;
+
+		foreach (bool vote in vootings)
+		{
+			if (vote)
+			{
+				yesVotes++;
+			}
+		}
+
+		Console.WriteLine($"Total Yes Votes: {yesVotes}");
+
+		Console.ReadKey();
+	}
+}
