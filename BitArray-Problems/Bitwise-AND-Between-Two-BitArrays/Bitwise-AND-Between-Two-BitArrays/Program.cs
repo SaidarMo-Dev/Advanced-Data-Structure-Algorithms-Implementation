50fc2ab81fcfcbf998fbf3e815fa676ecf678682
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-10 20:21:52 +0100

    Bitwise-AND-Between-Two-BitArrays

---
+++ BitArray-Problems/Bitwise-AND-Between-Two-BitArrays/Bitwise-AND-Between-Two-BitArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		BitArray a = new BitArray(new bool[] { true, false, false, true, true, true });
+		BitArray b = new BitArray(new bool[] { false, false, true, true, false, false });
+
+
+		var result = BitwiseAnd(a, b);
+
+
+		Console.WriteLine("Bitwise AND result: ");
+		for (int i = 0; i < result.Length; i++)
+		{
+			Console.Write(result[i] + " ");
+		}
+
+
+		Console.ReadKey();
+	}
+
+	public static BitArray BitwiseAnd(BitArray a, BitArray b)
+	{
+		if (a.Length != b.Length)
+			throw new ArgumentException("BitArrays must have the same length!");
+
+
+
+		BitArray result = new BitArray(a.Length);
+
+		for (int i = 0; i < a.Length; i++)
+		{
+			result[i] = a[i] & b[i];
+		}
+
+		// You  can also use the built-in method:  a.And(b);
+
+
+		return result;
+	}
+}
