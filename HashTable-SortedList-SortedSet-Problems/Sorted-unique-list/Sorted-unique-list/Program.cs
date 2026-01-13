f24737e2cd1854fce67b1150a57b694616343172
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-13 20:02:43 +0100

    Sorted-unique-list

---
+++ HashTable-SortedList-SortedSet-Problems/Sorted-unique-list/Sorted-unique-list/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		List<int> list = new List<int> { 45, 2, 2, 4, 66 };
+
+		SortedSet<int> sortedSet = new SortedSet<int>(list);
+
+		Console.WriteLine("Sorted Unique set :");
+		foreach (int i in sortedSet)
+		{
+			Console.Write(i + " ");
+		}
+
+		Console.WriteLine("\nPress any key to continue...");
+
+		Console.ReadKey();
+	}
+
+}
