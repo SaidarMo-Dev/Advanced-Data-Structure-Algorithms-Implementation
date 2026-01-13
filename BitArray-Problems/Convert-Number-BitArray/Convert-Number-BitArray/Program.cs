4b67791c5b3521727d1a79f17226a2153006c21b
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-13 19:31:39 +0100

    Convert-Number-BitArray

---
+++ BitArray-Problems/Convert-Number-BitArray/Convert-Number-BitArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+internal class Program
+{
+	private static BitArray ConvertNumberToBitArray(int number)
+	{
+
+		return new BitArray(new[] { number });
+
+	}
+	private static void Main(string[] args)
+	{
+		int number = 12;
+		var bits = ConvertNumberToBitArray(number);
+
+		bool leadingZero = true;
+
+		for (int i = bits.Length - 1; i >= 0; i--)
+		{
+			if (bits[i]) leadingZero = false;
+
+			if (!leadingZero)
+				Console.Write(bits[i] ? "1" : "0");
+		}
+
+		Console.ReadKey();
+	}
+}
