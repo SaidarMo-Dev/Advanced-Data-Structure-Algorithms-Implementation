9a05af347cf9d243bb9bff2ab95f3f251e1015e0
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-13 20:56:46 +0100

    Unique-items-chooping-card/

---
+++ HashTable-SortedList-SortedSet-Problems/Unique-items-chooping-card/Unique-items-chooping-card/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		SortedSet<string> items = new SortedSet<string>
+		{
+			"Orange",
+			"Apple",
+			"Orange", // Dublicate won't be added
+		};
+
+		Console.WriteLine("Items In Chooping card sorted alphabetical:");
+		foreach (string item in items)
+		{
+			Console.Write(item + " ");
+		}
+
+		Console.ReadKey();
+	}
+}
