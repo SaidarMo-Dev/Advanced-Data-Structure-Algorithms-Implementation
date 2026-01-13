fa4b6c2edc2e8415661e2b0f97aab9e6ce165dc0
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-13 20:02:15 +0100

    HashTable-Copy

---
+++ HashTable-SortedList-SortedSet-Problems/HashTable-Copy/HashTable-Copy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+internal class Program
+{
+
+	private static void Main(string[] args)
+	{
+		Hashtable ht1 = new Hashtable
+		{
+			{"Name", "Ali" },
+			{"Age", 23 },
+		};
+
+		Hashtable ht2 = new Hashtable(ht1);
+
+
+		Console.WriteLine("Contents of copied hashtable:");
+		foreach (DictionaryEntry entry in ht2)
+		{
+			Console.WriteLine($"{entry.Key} : {entry.Value}");
+
+		}
+
+
+		Console.ReadKey();
+
+	}
+}
