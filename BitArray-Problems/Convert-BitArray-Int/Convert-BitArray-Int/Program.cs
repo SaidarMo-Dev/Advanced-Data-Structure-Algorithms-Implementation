8e6c9de3942d64451f54c8eb58fe6ae685d9df50
SaidarMo-Dev <mohamalikdg@gmail.com>
2026-01-13 19:31:07 +0100

    Convert-BitArray-Int

---
+++ BitArray-Problems/Convert-BitArray-Int/Convert-BitArray-Int/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+internal class Program
+{
+	private static int ConvertBiArrayToInt(BitArray bits)
+	{
+		int result = 0;
+
+		for (int i = 0; i < bits.Length; i++)
+		{
+			if (bits[i])
+			{
+				result += 1 << i;
+			}
+		}
+
+
+		return result;
+	}
+	private static void Main(string[] args)
+	{
+		BitArray bits = new BitArray(new bool[] { true, true, true, false, true, true, false, false });
+
+		int number = ConvertBiArrayToInt(bits);
+
+
+		Console.WriteLine("After converting bits into number:" + number);
+
+
+		Console.ReadKey();
+	}
+}
